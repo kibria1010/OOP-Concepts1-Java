fc049520f6aa6f11b14d517a32cad90056f44af3
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-11-11 16:05:05 +0600

    Add files via upload

--- File.java
+++ File.java
@@ -1,9 +1,8 @@
 package oop;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.nio.file.*;
+import java.util.*;
 
 /**
  *
@@ -13,8 +12,10 @@ public class File {
 
     public static void main(String[] args) {
         readingFromFile();
-        readingFromFileUtil();
         writingToFile();
+        readingFromFileUtil();
+        appendToFileWithoutBuildingFunction();
+        findSumAndShowFromAFile();
     }
 
     public static void readingFromFile() {
@@ -23,10 +24,57 @@ public class File {
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
+            reader.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+    
+    public static void writingToFile() {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Append to the file and write 'exit' to exit.");
+        try (PrintWriter writer = new PrintWriter(new FileWriter("example.txt", true))) { // 'true' enables appending
+            String line;
+            while (!(line = in.readLine()).equals("exit")) {
+                writer.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void appendToFileWithoutBuildingFunction() {
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+        List<String> lines = new ArrayList<>();
+        
+        try {
+            reader = new BufferedReader(new FileReader("example.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            reader.close();
+            
+            writer = new PrintWriter(new FileWriter("example.txt"));
+            for (String s : lines) {
+                writer.println();
+            }
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Append to the file and write 'exit' to exit.");
+            while (!(line = reader.readLine()).equals("exit")) {
+                writer.println(line);
+            }
+            reader.close();
+            writer.close();
+            
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
     }
     
     public static void readingFromFileUtil() {
@@ -40,19 +88,22 @@ public class File {
             e.printStackTrace();
         }
     }
-    
-    public static void writingToFile() {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        try (PrintWriter writer = new PrintWriter(new FileWriter("example.txt", true))) { // 'true' enables appending
+
+    private static void findSumAndShowFromAFile() {
+        int sum=0;
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             String line;
-            while (!(line = in.readLine()).equals("0")) {
-                writer.println(line);
+            while ((line = reader.readLine()) != null) {
+                String[] sp = line.split(" ");
+                for (String word : sp) {
+                    sum += Integer.parseInt(word);
+                }
             }
+            reader.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
-        } finally {
-            
         }
+        System.out.println("sum: " + sum);
     }
 }
